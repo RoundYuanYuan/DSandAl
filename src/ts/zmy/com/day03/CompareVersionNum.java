4727f522fc62f7685e312583c891e4f7324ca031
RoundYuanYuan <mengyuan_per@outlook.com>
2018-12-03 14:44:34 +0800

    小改进

--- src/ts/zmy/com/day03/CompareVersionNum.java
+++ src/ts/zmy/com/day03/CompareVersionNum.java
@@ -13,7 +13,7 @@ public class CompareVersionNum {
 	 *         "1.0.1", version2 = "1" 输出: 1 示例 3: 输入: version1 = "7.5.2.4",
 	 *         version2 = "7.5.3" 输出: -1
 	 * 
-	 *         提交次数：2 优化了代码
+	 *         提交次数：3 优化性能
 	 */
 	public static int compareVersion(String version1, String version2) {
 		// 分割v1和v2按照 .
@@ -25,15 +25,15 @@ public class CompareVersionNum {
 		// v1和v2的指针
 		int v1Index = 0;
 		int v2Index = 0;
-
+		//讲字符窜转数字
+		int v1Num = 0;
+		int v2Num = 0;
 		/**
-		 * 1、判断v1和v2是否都没有超出数组范围 
-		 * 2、判断大小
+		 * 1、判断v1和v2是否都没有超出数组范围 2、判断大小
 		 */
-		
 		while (v1Len > v1Index && v2Len > v2Index) {
-			int v1Num = Integer.parseInt(v1Arr[v1Index]);
-			int v2Num = Integer.parseInt(v2Arr[v2Index]);
+			v1Num = Integer.parseInt(v1Arr[v1Index]);
+			v2Num = Integer.parseInt(v2Arr[v2Index]);
 			if (v1Num > v2Num)
 				return 1;
 			if (v1Num < v2Num)
@@ -52,16 +52,15 @@ public class CompareVersionNum {
 			return isAllZero(v1Arr, v1Len, v1Index, 1);
 		return 0;
 	}
-	
+
 	/**
-	 * 判断v1后边的数字是否都是0(包括 “00”，“0000”)
-	 * 是的话返回0
-	 * 否的话返回res
-	 * @param 	arr  	 要判断的数组
-	 * @param 	len 	 数组长度
-	 * @param 	vIndex 	 指针指向的位置
-	 * @param 	res  	 要返回的结果
-	 * @return 	res      同上
+	 * 判断v1后边的数字是否都是0(包括 “00”，“0000”) 是的话返回0 否的话返回res
+	 * 
+	 * @param arr    要判断的数组
+	 * @param len    数组长度
+	 * @param vIndex 指针指向的位置
+	 * @param res    要返回的结果
+	 * @return res 同上
 	 */
 	public static int isAllZero(String[] arr, int len, int vIndex, int res) {
 		int vNum = Integer.parseInt(arr[vIndex]);
